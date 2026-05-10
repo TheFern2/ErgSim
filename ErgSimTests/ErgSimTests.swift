65998d3fed1dac5109ce906db9ac7976722e9c2c
Fernando Balandran <fernandobe+git@protonmail.com>
2026-05-09 19:17:58 -0500

    init

---
+++ ErgSimTests/ErgSimTests.swift
@@ -0,0 +1,18 @@
+//
+//  ErgSimTests.swift
+//  ErgSimTests
+//
+//  Created by Fernando Balandran on 5/9/26.
+//
+
+import Testing
+
+struct ErgSimTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
